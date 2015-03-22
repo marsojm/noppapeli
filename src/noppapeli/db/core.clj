ad9970a20e0252d8c42688bb6e52f958a36ebf52
Marko Malinen <markojm1986@gmail.com>
2015-03-22 12:37:50 +0200

    Get db conf from environment variable

--- src/noppapeli/db/core.clj
+++ src/noppapeli/db/core.clj
@@ -3,9 +3,10 @@
     [yesql.core :refer [defqueries]]))
 
 (def db-spec
+  (or (System/getenv "DATABASE_URL")
   {:subprotocol "postgresql"
    :subname "//localhost/noppapeli"
    :user "db_user_name_here"
-   :password "db_user_password_here"})
+   :password "db_user_password_here"}))
 
 (defqueries "sql/queries.sql" {:connection db-spec})
